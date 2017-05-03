1ad8e8f90ca50ab687ccdf9b45b58e45303b9951
wangtao <wang.tao@cesgroup.com.cn>
2017-05-03 16:05:33 +0800

    commit coding files

---
+++ Hello/src/com/aob/common/Person.java
@@ -0,0 +1,18 @@
+package com.aob.common;
+
+/**
+ * Created by Windows User on 2017/5/3.
+ */
+public class Person {
+
+    private String name ;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "My name is " + this.name;
+    }
+}
---
+++ Hello/src/com/aob/test/Test.java
@@ -0,0 +1,14 @@
+package com.aob.test;
+
+import com.aob.common.Person;
+
+/**
+ * Created by Windows User on 2017/5/3.
+ */
+public class Test {
+
+    public  static  void  main (String[] args){
+
+        new Person("wangtao").toString();
+    }
+}
