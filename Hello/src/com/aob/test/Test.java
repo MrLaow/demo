cd69c988151c301795bd1265fd3a8e32dd0dd816
wangtao <wang.tao@cesgroup.com.cn>
2017-05-04 10:10:17 +0800

    modify name to wangtao

--- Hello/src/com/aob/test/Test.java
+++ Hello/src/com/aob/test/Test.java
@@ -9,6 +9,6 @@ public class Test {
 
     public  static  void  main (String[] args){
 
-        System.out.println(new Person("xuyn").toString()) ;
+        System.out.println(new Person("wangtao").toString()) ;
     }
 }
